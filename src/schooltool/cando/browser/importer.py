de2fb60bf38105908db22d80d704da4ac6d81de1
Alan Elkner <aelkner@gmail.com>
2012-04-19 01:56:44 -0400

    added Layers and Nodes sheets to importer

--- src/schooltool/cando/browser/importer.py
+++ src/schooltool/cando/browser/importer.py
@@ -22,8 +22,16 @@ Skills importer.
 
 from zope.traversing.browser.absoluteurl import absoluteURL
 
+from schooltool.cando.interfaces import ILayerContainer, INodeContainer
+from schooltool.cando.model import Layer, Node
 from schooltool.export.importer import ImporterBase
 from schooltool.export.importer import FlourishMegaImporter
+
+from schooltool.common import SchoolToolMessage as _
+
+
+ERROR_INVALID_PARENTS = _("has an invalid parent id")
+ERROR_INVALID_LAYERS = _("has an invalid layer id")
 
 
 class SkillsImporter(ImporterBase):
@@ -42,6 +50,99 @@ class ModelImporter(ImporterBase):
         raise NotImplemented()
 
 
+class LayersImporter(ImporterBase):
+
+    sheet_name = 'Layers'
+
+    def process(self):
+        sh = self.sheet
+        layers = ILayerContainer(self.context)
+
+        for row in range(1, sh.nrows):
+            if sh.cell_value(rowx=row, colx=0) == '':
+                break
+
+            num_errors = len(self.errors)
+            name = self.getRequiredTextFromCell(sh, row, 0)
+            title = self.getRequiredTextFromCell(sh, row, 1)
+            if num_errors < len(self.errors):
+                continue
+
+            if name in layers:
+                layers[name].title = title
+            else:
+                layers[name] = Layer(title)
+
+        for row in range(1, sh.nrows):
+            if sh.cell_value(rowx=row, colx=0) == '':
+                break
+
+            name = self.getRequiredTextFromCell(sh, row, 0)
+            parents = self.getTextFromCell(sh, row, 2)
+
+            layer = layers[name]
+            for parent in list(layer.parents):
+                layer.parents.remove(parent)
+            parts = [p.strip() for p in parents.split(',')]
+            for part in parts:
+                if part not in layers:
+                    self.error(row, 2, ERROR_INVALID_PARENTS)
+                    break
+                layer.parents.add(layers[part])
+
+
+class NodesImporter(ImporterBase):
+
+    sheet_name = 'Nodes'
+
+    def process(self):
+        sh = self.sheet
+        nodes = INodeContainer(self.context)
+        layers = ILayerContainer(self.context)
+
+        for row in range(1, sh.nrows):
+            if sh.cell_value(rowx=row, colx=0) == '':
+                break
+
+            num_errors = len(self.errors)
+            name = self.getRequiredTextFromCell(sh, row, 0)
+            title = self.getRequiredTextFromCell(sh, row, 1)
+            if num_errors < len(self.errors):
+                continue
+
+            if name in nodes:
+                nodes[name].title = title
+            else:
+                nodes[name] = Node(title)
+
+        for row in range(1, sh.nrows):
+            if sh.cell_value(rowx=row, colx=0) == '':
+                break
+
+            name = self.getRequiredTextFromCell(sh, row, 0)
+            parents = self.getTextFromCell(sh, row, 2)
+            node_layers = self.getTextFromCell(sh, row, 3)
+
+            node = nodes[name]
+            for parent in list(node.parents):
+                layer.parents.remove(parent)
+            for layer in list(node.layers):
+                node.layers.remove(layer)
+
+            parts = [p.strip() for p in parents.split(',')]
+            for part in parts:
+                if part not in nodes:
+                    self.error(row, 2, ERROR_INVALID_PARENTS)
+                    break
+                node.parents.add(nodes[part])
+            parts = [l.strip() for l in node_layers.split(',')]
+            for part in parts:
+                if part not in layers:
+                    self.error(row, 2, ERROR_INVALID_LAYERS)
+                    break
+                node.layers.add(layers[part])
+
+
 class SkillsMegaImporter(FlourishMegaImporter):
 
     def nextURL(self):
@@ -51,6 +152,9 @@ class SkillsMegaImporter(FlourishMegaImporter):
     @property
     def importers(self):
         return [
+            LayersImporter,
+            NodesImporter,
             SkillsImporter,
             ModelImporter,
             ]
+
