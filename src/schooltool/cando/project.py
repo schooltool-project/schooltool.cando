cbfdf89060851c92aa63c3fa5f2eb42b843dfb00
Alan Elkner <aelkner@gmail.com>
2012-07-30 23:36:24 -0400

    added cando external activities for project total and percent complete

--- src/schooltool/cando/project.py
+++ src/schooltool/cando/project.py
@@ -17,20 +17,28 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 """Gradebook projects."""
+from decimal import Decimal
+
 from persistent.dict import PersistentDict
 
 from zope.annotation.interfaces import IAnnotations
 from zope.interface import implements, implementer
 from zope.event import notify
-from zope.component import adapter
+from zope.component import (adapter, adapts, getUtility, getAdapters,
+    queryAdapter)
 from zope.container.contained import containedEvent
 from zope.container.interfaces import INameChooser
+from zope.i18n import translate
+from zope.intid.interfaces import IIntIds
 from zope.keyreference.interfaces import IKeyReference
 from zope.proxy import sameProxiedObjects
 
 from schooltool.requirement.requirement import Requirement
+from schooltool.requirement.scoresystem import UNSCORED
 from schooltool.gradebook.activity import Worksheets, Worksheet
-from schooltool.cando.interfaces import IProjects, IProject
+from schooltool.gradebook.interfaces import IExternalActivity
+from schooltool.gradebook.interfaces import IExternalActivities
+from schooltool.cando.interfaces import IProjects, IProject, IProjectsGradebook
 from schooltool.cando.interfaces import ICourseProjects, ICourseProject
 from schooltool.cando.skill import SkillSet
 from schooltool.course.interfaces import ISection, ICourse
@@ -167,3 +175,119 @@ def getSectionFromProjects(projects):
     annotations = projects.__parent__
     section = annotations.__parent__
     return section
+
+
+class CanDoExternalActivityBase(object):
+
+    implements(IExternalActivity)
+    adapts(IProject)
+
+    def __init__(self, context):
+        self.project = context
+        self.gradebook = IProjectsGradebook(context)
+        self.__parent__ = context
+        self.source = ""
+        self.external_activity_id = ""
+
+    @property
+    def description(self):
+        return self.project.description
+
+    def __eq__(self, other):
+        return IExternalActivity.providedBy(other) and \
+               self.source == other.source and \
+               self.external_activity_id == other.external_activity_id
+
+
+class CanDoExternalActivityTotal(CanDoExternalActivityBase):
+
+    @property
+    def title(self):
+        msg = _('${project} total points',
+            mapping={'project': self.project.title})
+        return translate(msg)
+
+    def getGrade(self, student):
+        numComps = totalPoints = 0
+        for competency in self.project.values():
+            ev = self.gradebook.getScore(student, competency)
+            if ev is None or ev.value == UNSCORED:
+                continue
+            value = ev.scoreSystem.getNumericalValue(ev.value)
+            numComps += 1
+            totalPoints += value
+        numCompsTimes3 = numComps * 3
+        if numComps:
+            return Decimal(totalPoints) / Decimal(numCompsTimes3)
+        return None
+
+
+class CanDoExternalActivityPercentComplete(CanDoExternalActivityBase):
+
+    @property
+    def title(self):
+        msg = _('${project} percent complete',
+            mapping={'project': self.project.title})
+        return translate(msg)
+
+    def getGrade(self, student):
+        numComps = totalComplete = 0
+        for competency in self.project.values():
+            if not competency.required:
+                continue
+            numComps += 1
+            ev = self.gradebook.getScore(student, competency)
+            if ev is None or ev.value == UNSCORED:
+                continue
+            if ev.scoreSystem.isPassingScore(ev.value):
+                totalComplete += 1
+        if numComps:
+            return Decimal(totalComplete) / Decimal(numComps)
+        return None
+
+
+class CanDoExternalActivities(object):
+
+    implements(IExternalActivities)
+
+    title = u"CanDo"
+    source = "cando.external_activities"
+
+    def __init__(self, context):
+        self.context = context
+        self.__parent__ = context
+
+    def getExternalActivities(self):
+        result = []
+        intids = getUtility(IIntIds)
+        section_projects = IProjects(self.context)
+        for project in section_projects.values():
+            for name, activity in getAdapters([project], IExternalActivity):
+                external_activity = activity
+                external_activity.source = self.source
+                project_external_id = '%s_%s' % (name, intids.getId(project))
+                external_activity.external_activity_id = project_external_id
+                result.append(external_activity)
+        return result
+
+    def getExternalActivity(self, external_activity_id):
+        parts = external_activity_id.split('_')
+        if len(parts) != 2:
+            return None
+        name, project_intid = parts
+        try:
+            project_intid = int(project_intid)
+        except (ValueError,):
+            return None
+
+        project = getUtility(IIntIds).queryObject(project_intid)
+        if project is None:
+            return None
+        activity = queryAdapter(project, IExternalActivity, name=name)
+        if activity is None:
+            return None
+
+        activity.source = self.source
+        activity.external_activity_id = external_activity_id
+        return activity
+
