46f33ed86668a4161afd39543e0b4ab5680dfbb9
Douglas Cerna <douglascerna@yahoo.com>
2013-04-08 07:28:36 -0600

    Changed Skills selector to Skill Sets in the gradebook (LP: #1154425)

--- src/schooltool/cando/browser/gradebook.py
+++ src/schooltool/cando/browser/gradebook.py
@@ -308,7 +308,7 @@ class CanDoModesViewlet(flourish.viewlet.Viewlet):
         if ISectionSkills(section):
             result.append({
                     'id': 'skills',
-                    'label': _('Skills'),
+                    'label': _('Skill Sets'),
                     'url': section_url + '/gradebook-skills',
                     'selected': ISkillsGradebook.providedBy(self.context),
                     })
