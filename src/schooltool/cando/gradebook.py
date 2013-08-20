e064de555d3c6ef326fcc0217d91a87016bb6f41
Justas Sadzevicius <justas@pov.lt>
2013-08-20 13:31:52 +0300

    Make gradebook's __parent__ a property

--- src/schooltool/cando/gradebook.py
+++ src/schooltool/cando/gradebook.py
@@ -272,9 +272,9 @@ class CanDoStudentGradebook(StudentGradebook):
     implements(ICanDoStudentGradebook)
     adapts(IBasicPerson, ICanDoGradebook)
 
-    def __init__(self, *args, **kw):
-        super(CanDoStudentGradebook, self).__init__(*args, **kw)
-        self.__parent__ = self.gradebook.__parent__
+    @property
+    def __parent__(self):
+        return self.gradebook.__parent__
 
 
 class CanDoStudentGradebookTraverser(object):
